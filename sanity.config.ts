b60a6a64d21a324fd499c2edbb1350d906bfb791
lucasgdb <lucasgdbittencourt@gmail.com>
2022-12-24 17:01:31 -0300

    chore: change old method to new one

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,8 +1,8 @@
-import { createConfig } from 'sanity';
+import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { markdownSchema } from 'sanity-plugin-markdown';
 
-export default createConfig({
+export default defineConfig({
   name: 'default',
   title: 'lucasbittencourt.dev',
   projectId: 'df9zy4hk',
