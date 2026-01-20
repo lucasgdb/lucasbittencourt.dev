4d25d7a618c5df31778ff0b2a9c94cc807920a27
lucasgdb <lucasgdbittencourt@gmail.com>
2026-01-20 16:22:42 -0300

    test

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -10,6 +10,8 @@ export default function Page() {
     <section>
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
       <BlogPosts />
+      <p>djsklafdsjklfj</p>
+      <img  src="https://hub-ui.nonprod.crewhu.com/splash/iPhone_13_mini__iPhone_12_mini__iPhone_11_Pro__iPhone_XS__iPhone_X_portrait.png"/>
     </section>
   );
 }
